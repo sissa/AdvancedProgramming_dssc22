7af433bd309cb888d3d60860c77a732a054435fc
Irina Davydenkova <cnhtkf@yandex.ru>
2022-10-04 18:45:50 +0200

    added an example for types

---
+++ examples/types.cpp
@@ -0,0 +1,14 @@
+#include <iostream>     
+#include <limits>       // for std::numeric_limits
+
+int main () {
+  bool var{true}; //or false
+  double x{3.5};
+  float  xx{3.5f}; //if precision and big numbers are not needed
+  int a{1};
+  short int b{2};
+  long int c{3};
+  long long int d{4};
+  char s{'a'};
+  return 0;
+} 
