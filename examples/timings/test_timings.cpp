2d611c145606a18cd17b4a88f26df5a8ac5ed2b4
Irina Davydenkova <cnhtkf@yandex.ru>
2022-11-16 14:13:46 +0100

    added some more info into timings file

--- examples/timings/test_timings.cpp
+++ examples/timings/test_timings.cpp
@@ -13,11 +13,19 @@ int main(){
     
     auto t_end = std::chrono::steady_clock::now();
     
-   
-    auto total = std::chrono::duration_cast<std::chrono::microseconds>(t_end - t_start).count();
+    //the difference returns another type, not time_point
+    auto total = std::chrono::duration_cast<std::chrono::milliseconds>(t_end-t_start).count();
     
      //microseconds are to small, not too reliable, so we get 2000+
     std::cout<<"we have measured: "<<total<<std::endl;
+    
+    //this is how you can see what t_start (it's when you last rebooted)
+    std::cout<<std::chrono::duration_cast<std::chrono::hours>(t_start.time_since_epoch()).count()<<std::endl;
+    
+    //let's see how the system clock differs
+    //the "epoch" for it is Thursday, 1 January 1970
+    auto t_test = std::chrono::system_clock::now();
+    std::cout<<std::chrono::duration_cast<std::chrono::hours>(t_test.time_since_epoch()).count()<<std::endl;
     
     return 0;
 }
